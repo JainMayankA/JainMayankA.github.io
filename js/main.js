0d3950d1b9e10346e59958e512bba6ca18ea5fd7
Mayank Jain <maxjain71@gmail.com>
2021-03-23 18:07:26 -0500

    Github Calendar Plugin

--- js/main.js
+++ js/main.js
@@ -120,11 +120,15 @@ jQuery(document).ready(function($) {
     );
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    // new GitHubCalendar("#github-graph", "JainMayankA");
+    GitHubCalendar(".calendar", "JainMayankA", {
+        responsive: true,
+        tooltips: true
+    });
 
 
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    // GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
 
 
 });
